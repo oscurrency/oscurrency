2a41093aebcbf1acb30442fc3652988af1ff984a
ktopolski <karoltopolski92@gmail.com>
2014-02-07 11:22:55 +0100

    added comment for cron statement

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -2,6 +2,7 @@
 # Lockfile feature only works on OSes that support the flock (man 2 flock) call.
 # scheduler = Rufus::Scheduler.new(:lockfile => ".task-scheduler.lock")
 # 
+# At the end of month on 11:00 pm.
 # scheduler.cron '0 23 L * *' do
   # begin
     # Account.calculate_demmurage()
