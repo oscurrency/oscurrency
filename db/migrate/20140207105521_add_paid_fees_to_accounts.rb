601c75e701dee6c1d7941f2aa7162a166db5d657
ktopolski <karoltopolski92@gmail.com>
2014-02-10 17:18:44 +0100

    Transactions should be calculated based on Exchange class, so we don't need paid_fees column. - fix #552

--- db/migrate/20140207105521_add_paid_fees_to_accounts.rb
+++
@@ -1,9 +0,0 @@
-class AddPaidFeesToAccounts < ActiveRecord::Migration
-  def self.up
-    add_column :accounts, :paid_fees, :decimal, :precision => 8, :scale => 2, :default => 0
-  end
-  
-  def self.down
-    remove_column :accounts, :paid_fees
-  end
-end
