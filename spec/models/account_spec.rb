422dd645ebd4639a91194f3e6b6671e1e73b7dd1
ktopolski <karoltopolski92@gmail.com>
2014-02-13 09:36:02 +0100

    Little tests cleanup.

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -12,31 +12,6 @@ describe Account do
     end
     
   end
-  
-  before do
-    @fees = [ fees(:tc_perc_tran), fees(:tc_tran), fees(:tc_perc_monthly), fees(:tc_monthly), fees(:tc_perc_yearly), fees(:tc_yearly),
-              fees(:cash_perc_tran), fees(:cash_tran), fees(:cash_perc_monthly), fees(:cash_monthly), fees(:cash_perc_yearly), fees(:cash_yearly),
-              fees(:mixed_tc_perc), fees(:mixed_tc_tran), fees(:mixed_tc_perc_monthly), fees(:mixed_tc_monthly),
-              fees(:mixed_cash_perc), fees(:mixed_cash_tran), fees(:mixed_cash_perc_monthly), fees(:mixed_cash_monthly)
-            ]
-    @plans = [ plan_types(:transactions), plan_types(:monthly), plan_types(:yearly), plan_types(:cash_tran), plan_types(:cash_mon), plan_types(:cash_yearly), plan_types(:mixed_tc_tran_mon) ]
-    @people = [ people(:aaron), people(:quentin), people(:buzzard), people(:kelly), people(:admin), people(:reserve), people(:mixed_test) ]
-    @accounts = [ accounts(:aarons), accounts(:quentins), accounts(:buzzards), accounts(:kellys), accounts(:admins), accounts(:reserves), accounts(:mixed) ]
-    @group = groups(:one)
-    @memberships = [ memberships(:one), memberships(:two), memberships(:three), memberships(:four) ]
-    @exchanges = [ exchanges(:one), exchanges(:two), exchanges(:three), exchanges(:four), exchanges(:five)]
-    begin
-      @fees.each do |f|; f.save!; end
-      @plans.each do |p|; p.save!; end
-      @people.each do |pp|; pp.save!; end
-      @accounts.each do |a|; a.save!; end
-      @group.save!
-      @memberships.each do |m|; m.save!; end
-      @transaction.save! 
-    rescue => e
-      p "Exception: #{e.to_s}"
-    end
-  end
     
    describe "scheduled task" do
      
@@ -73,7 +48,7 @@ describe Account do
        # notes: Quentin working for Mixed.
        fees = Account.find_by_name("mixed").fees_invoice_for_month(Date.today.month, Date.today.year)
        # 10 per transaction + 10 monthly + 40% per transaction + 40% from sum of all transactions in the month
-       fees[:"trade_credits"].should == 28.0
+       fees[:"trade-credits"].should == 28.0
        fees[:cash].to_f.should == 28.0
      end
      
