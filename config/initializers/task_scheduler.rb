667ffe21d86dcdb58fdd5d9e47afc3bc762057fe
ktopolski <karoltopolski92@gmail.com>
2014-02-21 11:26:25 +0100

    Task for billing credit cards at the end of week - fix #453

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,28 +1,40 @@
 # Only one instance of scheduler at the time. 
 # Lockfile feature only works on OSes that support the flock (man 2 flock) call.
-# scheduler = Rufus::Scheduler.new(:lockfile => ".task-scheduler.lock")
+scheduler = Rufus::Scheduler.new(:lockfile => ".task-scheduler.lock")
 # 
+
+# At the end of each week of every month on 23:30 pm  -it may take some time.
+scheduler.cron '30 23 W * 0' do
+  begin
+    Account.pay_transaction_cash_fees
+  rescue => e
+    $stderr.puts '-' * 80
+    $stderr.puts e.message
+    $stderr.puts e.stacktrace
+    $stderr.puts '-' * 80
+  end
+end 
+
 # At the end of month on 21:00 pm - it may take some time.
-# scheduler.cron '0 21 L * *' do
-  # begin
-    # Account.pay_transaction_cash_fees
-    # Account.pay_monthly_fees
-  # rescue => e
-    # $stderr.puts '-' * 80
-    # $stderr.puts e.message
-    # $stderr.puts e.stacktrace
-    # $stderr.puts '-' * 80
-  # end
-# end
+scheduler.cron '0 21 L * *' do
+  begin
+    Account.pay_monthly_fees
+  rescue => e
+    $stderr.puts '-' * 80
+    $stderr.puts e.message
+    $stderr.puts e.stacktrace
+    $stderr.puts '-' * 80
+  end
+end
 
 # At the end of year on 21:00 pm. End of year is end of 12th month.
-# scheduler.cron '0 21 L 12 *' do
-  # begin
-    # Account.pay_yearly_fees
-  # rescue => e
-    # $stderr.puts '-' * 80
-    # $stderr.puts e.message
-    # $stderr.puts e.stacktrace
-    # $stderr.puts '-' * 80
-  # end
-# end
+scheduler.cron '0 21 L 12 *' do
+  begin
+    Account.pay_yearly_fees
+  rescue => e
+    $stderr.puts '-' * 80
+    $stderr.puts e.message
+    $stderr.puts e.stacktrace
+    $stderr.puts '-' * 80
+  end
+end
