918182ce24be08c3dfa8d5b79a850877ffaa1c19
ktopolski <karoltopolski92@gmail.com>
2014-03-06 11:20:32 +0100

    Stripe init for testing

--- config/initializers/stripe_settings.rb
+++ config/initializers/stripe_settings.rb
@@ -1,6 +1,13 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUB_KEY'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
-}
+unless Rails.env == "production"
+  Rails.configuration.stripe = {
+    :publishable_key => "pk_test_X9TPxSlTGcaBwR3KasYLTm9T",
+    :secret_key      => "sk_test_NQ5VD1mm7xgVkgoRPXCSYTHh"
+  }
+else
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['STRIPE_PUB_KEY'],
+    :secret_key => ENV['STRIPE_SECRET_KEY']
+  }
+end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
